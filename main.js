0859212c4fed409c2fc328fcb1ed2852dcc82db7
0n3W4y <vulturetm@gmail.com>
2016-03-25 15:43:46 +0300

    work harder, my friend.
    go home, yankee

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var set_component_in = function(object, className, id, component){
 	var p = object,
 		k = className,
 		v = id,
-		c = component,
-		x = p[k];
-	x[v] = c;
+		c = component;
+		p[k] = (p[k] === "undefined") ? {} : p[k];
+
 	return;
 };
 
@@ -17,6 +17,13 @@ var get_component_in = function(object, className, id){
 
 //-------------------------------------------------------------------------------------------------------------------------||
 
+var CommonDeath = Trait.inherit({
+	__className: "CommonDeath",
+
+	death: function(){
+
+	}
+});
 
 var CommonShoot = Trait.inherit({
 	__className: "CommonShoot",
@@ -39,7 +46,7 @@ var CommonWalk = Trait.inherit({
 var CommonTick = Trait.inherit({
 	__className: 'CommonTick',
 
-	fps: 60,
+	fps: 30,
 	loopId: null,
 
 	startLoop : function() {
@@ -57,7 +64,7 @@ var CommonTick = Trait.inherit({
 		}
 	},
 
-	paused: null,
+	paused: undefined,
 	togglePause : function() {
 		this.paused = !this.isPaused();
 	},
@@ -70,15 +77,19 @@ var CommonTick = Trait.inherit({
 		return $.now();
 	},
 
-	lastTick: null,
-	time: function() {
-		return this.lastTick;
-	},
-
-
+	lastTick: 0,
 	tick: function() {
-
-	},
+		if (this.paused){
+			return;
+		}
+		var time = this.now();
+		var delta = time - this.lastTick;
+		if (delta > 64) { // ~2*1000/this.fps;
+			delta = 32; // ~1000/this.fps;
+		}
+		this.update(delta);
+		this.lastTick = time;
+	}
 });
 
 var CommonComponent = Object.inherit(
@@ -88,31 +99,38 @@ var CommonComponent = Object.inherit(
 
 
 	initialize: function(data){
+		console.log("initialize started");
 		this.commonInit(data);
 	},
 
 	commonInit: function(data){
 		this.id = data.id ? data.id : this.createId();
 		this.name = data.name;
-		this.parent = data.parent || this;
+		this.parent = data.parent;
 		this.components = this.components ? this.components : {};
+		console.log("commonInit done");
 	},
 
 	createComponent: function(classDefenition, data){
-		var classDef = window[classDefenition];
-		var className = classDef.prototype.__className;
-		var component = new classDef(data);
-		set_component_in(this.components, classDef, component.id, component);
+		//var classDef = window[classDefenition];
+		var className = classDefenition.prototype.__className;
+		var component = new classDefenition(data);
+		if (!data.parent){
+			data.parent = this;
+		}
+		set_component_in(this.components, className, component.id, component);
+		console.log("create component done, created: " + classDefenition + ", " + data.name);
 		return component;
 	},
 
-	removeComponent: function(){
-
+	removeComponent: function(classDefenition, id){
+		var componentId = get_component_in(classDefenition, id);
 	},
 
 	createId: function(classDefenition){ // спизжено, нужно разобраться)
 		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+			console.log("id created");
 			return v.toString(16);
 		});
 	}
@@ -122,16 +140,32 @@ var CommonComponent = Object.inherit(
 var Player = CommonComponent.inherit(
 	CommonShoot,
 	CommonWalk,
+	CommonDeath,
 {
 	__className: "Player",
 
-	name: "NickName",
+	name: "NickName"
 
 });
 
 var World = CommonComponent.inherit(
 	CommonTick,
 {
-	__className: "World"
+	__className: "World",
 
+	update: function(delta){
+
+	}
+});
+
+var world = new World({id:"main", name:"DesertHiils", parent:window});
+var playerOne = world.createComponent(Player, {name:"player1"});
+
+$.document.ready(function(){
+	$("input.pause").click(function(){ 
+		var attribute = $(this).attr("value") == "pause" ? "unpause" : "pause";
+		$(this).attr("value", attribute);
+		return "wolrd.togglePause";
+	});
+	$("input.start").click("world.startLoop");
 })
