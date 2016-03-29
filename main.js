54906a01836c249271916fcc7f5a3b2fb6156d5c
0n3W4y <vulturetm@gmail.com>
2016-03-29 16:15:19 +0300

    now bot's seek and walk to each other :D! WoW!

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ var get_component_in = function(object, className, id){
 	return o;
 };
 
-var randomMovePoint = function(player){
+var randomMovePoint = function(player, delta){
 	var pointX, pointY;
 	var p = player;
 	var point = player.currentPoint;
@@ -40,10 +40,12 @@ var randomMovePoint = function(player){
 		pointY = Math.round(Math.random()*worldMapY);
 	}else{
 		var a = Math.round(Math.random()) ? p.velocity : -p.velocity;
+		var b = Math.round(Math.random()) ? p.velocity : -p.velocity;
 		var pX = point.x;
 		var pY = point.y;
-		pointX = pX + a;
-		pointY = pY + a;
+
+		pointX = pX + a/delta;
+		pointY = pY + b/delta;
 		pointX = (pointX <= 100 && pointX >= 0) ? pointX : pX;
 		pointY = (pointY <= 100 && pointY >= 0) ? pointY : pY;
 	}
@@ -60,8 +62,99 @@ var randomSortingForArray = function(a, b){
 
 //-------------------------------------------------------------------------------------------------------------------------||
 
+var GameAI = Trait.inherit({
+	__className: "GameAI",
+
+	findClosestEnemy: function(players){
+		var allEnemies = players;
+		var enemyPoints = [];
+		var result = [];
+		for (var i = 0; i < allEnemies.length; i++){
+			var a = allEnemies[i];
+			enemyPoints.push(a.currentPoint);
+		}
+		for (var j = 0; j < enemyPoints.length; j++){
+			var b = enemyPoints[j];
+			var distanceX = b.x - this.currentPoint.x;
+			var distanceY = b.y - this.currentPoint.y;
+			distanceX = (distanceX > 0) ? distanceX : -distanceX;
+			distanceY = (distanceY > 0) ? distanceY : -distanceY;
+			var distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+			result.push(distance);
+		}
+		var enemy = Math.min(...result);
+		var enemyId = result.indexOf(enemy);
+		return allEnemies[enemyId];
+	},
+
+	findAllPlayers: function(){
+		var o = this.parent.components.Player;
+		var enemyArr = [];
+		for (var key in o){
+			var a = o[key];
+			if (a.name != this.name){
+				enemyArr.push(a);
+			}
+		}
+		return enemyArr;
+	},
+
+	findPath: function(enemy){
+		var enemyPoint = enemy.currentPoint;
+		var positionX = Math.round(this.currentPoint.x) - Math.round(enemyPoint.x);
+		var positionY = this.currentPoint.y - enemyPoint.y;
+		var stepX = (positionX > 0) ? -1 : 1;
+		if ( Math.round(positionX) == 0){
+			stepX = 0;
+		}
+		var stepY = (positionY > 0) ? -1 : 1;
+		if ( Math.round(positionY) == 0){
+			stepY = 0;
+		}
+		return [stepX, stepY];
+	},
+
+	aiLogic: function(delta){
+		if (!this.currentPoint){
+			this.spawn();
+			return;
+		};
+
+		var allPlayers = this.findAllPlayers();
+		if (allPlayers){
+			var enemy = this.findClosestEnemy(allPlayers);
+			var path = this.findPath(enemy);
+		}else{
+			return;
+		};
+
+		this.move(delta, path);
+	}
+
+
+});
+
+var GameSpawn = Trait.inherit({
+	__className: "GameSpawn",
+
+	spawn: function(){
+		var worldMapX = this.parent.gameMap.x;
+		var worldMapY = this.parent.gameMap.y;
+		var pointX = Math.round(Math.random()*worldMapX);
+		var pointY = Math.round(Math.random()*worldMapY);
+		this.currentPoint = {x: pointX, y: pointY};
+		this.onSpawn();
+		return;
+	},
+
+	onSpawn: function(){
+		var rightNow = this.timeToConsole();
+		console.log(rightNow + this.name + " spawned on [x=" + this.currentPoint.x + "; y=" + this.currentPoint.y + "]");
+	}
+})
+
 var GameDeath = Trait.inherit({
-	__className: "CommonDeath",
+	__className: "GameDeath",
 
 	death: function(){
 
@@ -69,7 +162,7 @@ var GameDeath = Trait.inherit({
 });
 
 var GameShoot = Trait.inherit({
-	__className: "CommonShoot",
+	__className: "GameShoot",
 
 
 	shootSpeed: 1000, // 1 times per second
@@ -79,7 +172,7 @@ var GameShoot = Trait.inherit({
 
 	onShoot: function(target){
 		var rightNow = this.timeToConsole();
-		console.log(rightNow + this.name + " shooted to " + target.name + ", and ...");// + "miss/hit"
+		console.log(rightNow + this.name + " shooted to " + target.name + ", and miss/hit* ");
 	},
 
 	clip: 12,
@@ -93,30 +186,31 @@ var GameShoot = Trait.inherit({
 });
 
 var GameWalk = Trait.inherit({
-	__className: "CommonWalk",
+	__className: "GameWalk",
 
 	velocity: 1, // 1 - step, 2 - run
-	moveSpeed: 1000, // 1 times per second
+	moveSpeedLog: 2000, 
 	deltaVelocity: 0,
 	currentPoint: null,
-	move: function(delta){
-		if (this.deltaVelocity >= this.moveSpeed){
-			var point = randomMovePoint(this);
-			var lastPoint = (this.currentPoint) ? this.currentPoint : null;
-			this.currentPoint = point;
-			this.onMove(lastPoint);
+	move: function(delta, path){
+		var dirX = path[0];
+		var dirY = path[1];
+		var pX = this.currentPoint.x;
+		var pY = this.currentPoint.y;
+		var a = this.velocity/delta;
+		var pointX = pX + dirX*a;
+		var pointY = pY + dirY*a;
+		this.currentPoint = {x: pointX, y: pointY};
+		if (this.deltaVelocity >= this.moveSpeedLog){
+			this.onMove();
 			this.deltaVelocity = 0;
 		}
 		this.deltaVelocity += delta;
 	},
 
-	onMove: function(lastPoint){ //log
+	onMove: function(){ //log
 		var rightNow = this.timeToConsole();
-		if (!lastPoint){
-			console.log(rightNow + this.name + " spawned on [x=" + this.currentPoint.x + "; y=" + this.currentPoint.y + "]");
-		}else{
-			console.log(rightNow + this.name + " was moved out from [x=" + lastPoint.x + "; y= " + lastPoint.y +"] -> to: [x=" + this.currentPoint.x + "; y=" + this.currentPoint.y + "]");
-		}
+		console.log(rightNow + this.name + " was moved out to: [x=" + Math.round(this.currentPoint.x) + "; y=" + Math.round(this.currentPoint.y) + "]");
 	}
 });
 
@@ -175,26 +269,31 @@ var CommonComponent = Object.inherit(
 	__className: "CommonComponent",
 
 
-	initialize: function(data){
-		console.log("initialize started");
-		this.commonInit(data);
+	initialize: function(data, params){
+		console.log("initialize started...");
+		this.commonInit(data, params);
 	},
 
-	commonInit: function(data){
+	commonInit: function(data, params){
 		this.id = data.id ? data.id : this.createId();
 		this.name = data.name;
 		this.parent = data.parent;
 		this.components = this.components ? this.components : {};
-		console.log("commonInit done");
+		if (params){
+			for (var key in params){
+				this[key] = params[key];
+			}
+		}
+		console.log("commonInit done.");
 	},
 
-	createComponent: function(classDefenition, data){
+	createComponent: function(classDefenition, data, params){
 		//var classDef = window[classDefenition];
 		var className = classDefenition.prototype.__className;
 		if (!data.parent){
 			data.parent = this;
 		}
-		var component = new classDefenition(data);
+		var component = new classDefenition(data, params);
 		set_component_in(this.components, className, component.id, component);
 		console.log("create component done, created: " + className + ", with name:" + data.name);
 		return component;
@@ -219,7 +318,7 @@ var CommonComponent = Object.inherit(
 		var mm = date.getMinutes();
 		var ss = date.getSeconds();
 		var ms = date.getMilliseconds();
-		return "[" + hh + ":" + mm + ":" + ss + ":" + ms + "]";
+		return "[" + hh + ":" + mm + ":" + ss + ":" + ms + "] ";
 	}
 
 });
@@ -228,6 +327,8 @@ var Player = CommonComponent.inherit(
 	GameShoot,
 	GameWalk,
 	GameDeath,
+	GameAI,
+	GameSpawn,
 {
 	__className: "Player",
 
@@ -242,46 +343,23 @@ var World = CommonComponent.inherit(
 
 	gameMap: {x: 100, y: 100},
 	update: function(delta){
-		this.moveAllPlayers(delta);
-		this.shootAllPlayers(delta);
+		this.runAI(delta);
 	},
 
-	moveAllPlayers: function(delta){
+	runAI: function(delta){
 		var o = this.components.Player
 		for (var key in o){
 			var p = o[key];
-			p.move(delta);
+			p.aiLogic(delta);
 		}
-	},
-
-	shootAllPlayers: function(delta){ 
-		if (this.shootDelta > 1500){ // one time per halfsecond;
-			var arr = [];
-			this.shootDelta = 0;
-			var o = this.components.Player
-			for (var key in o){
-				var p = o[key];
-				arr.push(p);
-			}
-			arr.sort(randomSortingForArray);
-			for (var i = 0; i < arr.length; i++){
-				for (var j = 0; j < 1;){
-					var a = Math.round(Math.random()*(arr.length-1));
-					if (a != i){
-						j = 1;
-					}
-				}
-				arr[i].shoot(arr[a]);
-			}
-		}
-		this.shootDelta += delta;
 	}
+
 });
 
 var world = new World({id:"main", name:"DesertHiils", parent:window});
 var playerOne = world.createComponent(Player, {name:"player1"});
 var playerTwo = world.createComponent(Player, {name:"player2"});
-var playerThree = world.createComponent(Player, {name:"player3", velocity: 2}); //test
+var playerThree = world.createComponent(Player, {name:"SpeedRunningBot"}, {velocity: 2}); //test
 
 $(document).ready(function(){
 	$("input.pause").click(function(){
